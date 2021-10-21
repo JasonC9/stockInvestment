3b167ea0d8ecf3dbe6de7b6a38a3939b2956d439
Jason Oh <jkoh123@yahoo.com>
2021-10-21 16:10:53 -0400

    I have updated AdminPage since Jason Lam has updatedHomePage so AdminPage is working properly again

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,15 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import Card from '../components/Card';
 import Navbar from '../components/navbar';
 import { isAuthenticated } from '../utilities/authenticationUtils';
 import { Grid } from "@material-ui/core";
+import {InfoCard} from '../components/CardElements';
 import AdminNavbar from '../components/AdminNavbar';
 
 
-export default function HomePage() {
+export default function AdminPage(props:any) {
+
+    function renderTable(cards:any)
+    {
+        return(
+            Object.entries(cards).forEach((fieldStock) => 
+            <Card stock={fieldStock}/>)
+        )
+    }
 
     const history = useHistory();
+
+
+    const testStock = {
+        stock_id: 1,
+        stockName: "Company Name",
+        description: "This is a fun company please give us money! The company was founded by me when I was just 12 years old.",
+        price: 2,
+        img: "This is the image",
+        marketCap: 2102390,
+        totalInvested: 1230012
+    }
+    let cards: any[]=[];
+
+
 
     useEffect(() => {
         (async () => {
@@ -20,7 +43,28 @@ export default function HomePage() {
         })();
     }, []);
 
+    const [stocks, setStocks] = useState([])
 
+
+    useEffect(() => {
+        let url = `/stock/findAll`;
+        fetch(url,
+            {
+                method: "GET",
+                credentials: "include",
+            }).then(response => response.json())
+            .then(cards =>{setStocks(cards)
+            });
+            },[]);
+
+            // let checkStock=useState(testStock);
+            // const list=()=>{
+            //     return cards.map((checkStock)=>{
+            //         return(<div>Test{checkStock.stock_id}Test</div>)
+            //     })
+            // }
+
+            console.log(stocks)
     return (
         <div>
             <div style={{
@@ -28,6 +72,8 @@ export default function HomePage() {
                 backgroundRepeat: `no-repeat`,
                 backgroundSize: `1920px 720px`
             }}>
+
+
             <AdminNavbar />
             <p>
                 This is the home page.
@@ -36,24 +82,49 @@ export default function HomePage() {
             <br />
             <Link to="/mjinvestments/stocktest"> to stock test page</Link>
 
-
-
+            <InfoCard>What is MJ Investments?
+                <p>We are a third party marketing website that offers services to buy and sell stocks for you and your company</p>
+            </InfoCard>
+            <br/>
             <Grid container spacing={4}>
-            <Grid item xs={6} sm={6} md={4}>
-            <Card />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-            <Card />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-            <Card />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-            <Card />
-            </Grid>
+
+            {/* <div className="stockI">
+            {
+            cards.map((arrStock)=>(
+                <Card stock_id={arrStock.stock_id}/>
+            ))}
+            </div> */}
+
+
+
+            {
+                stocks.map((stock)=>{return(
+                    <Grid item xs={6} sm={6} md={4}>
+                    <Card stock={stock}/>
+                    </Grid>
+                    )})
+
+            }
+
+
+
+
+            {/* {
+                renderTable(stocks);
+            } */}
             </Grid>
 
 
+            
+
+
+            {/* <div>
+                {
+                    cards.map(({testStock})=>(
+                        <Card stock_id={testStock}></Card>
+                    ))
+                }
+                </div> */}
             </div>
         </div>
     )
